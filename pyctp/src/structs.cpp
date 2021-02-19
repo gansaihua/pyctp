f3a4a8b9e7114d9b8d3760a0388ce69c88971fb6
gansaihua <gansaihua@outlook.com>
2021-02-19 15:46:16 +0800

    remove redundant imports

--- pyctp/src/structs.cpp
+++ pyctp/src/structs.cpp
@@ -1,10 +1,16 @@
 ﻿#include "ThostFtdcUserApiStruct.h"
-#include "encoding.h"
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
 namespace py = pybind11;
+
+
+inline py::str encoding_cast(const char a[], const char* encoding = "gb2312") {
+	std::string s = a;
+	PyObject* o = PyUnicode_Decode(s.data(), s.length(), encoding, "Decode fail.");
+	return py::reinterpret_borrow<py::str>(o);
+};
 
 
 void init_structs(py::module_& m) {
